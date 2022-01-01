62c37e6bad001e17d53ba7f8421b596d9121b9f7
DanWillisUni <dan.willis.uni@gmail.com>
2022-01-01 18:37:54 +0000

    getting frequencies through to python

--- src/PartTwo/main.py
+++ src/PartTwo/main.py
@@ -1,12 +1,14 @@
-import AverageLateness
-import DB
+import ProbabilityHelpers as ph
 
 connStr = "Server=DANS-LATITUDE\SQLEXPRESS;Database=AIChatBot;Trusted_Connection=yes;"
 #connStr = "Server=DANS-LATITUDE\SQLEXPRESS;Database=AIChatBot;Trusted_Connection=yes;" Charlies
 #connStr = "Server=DANS-LATITUDE\SQLEXPRESS;Database=AIChatBot;Trusted_Connection=yes;" Brandons
 
-print(AverageLateness.getAverageLateness(connStr,"SOTON","WATRLMN",10))
-
-'''stations = DB.runQuery(connStr,"EXEC LookupStation @searchTerm = 'LONDON'")
-for station in stations:
-    print(station.split(",")[0])'''
+allLate = ph.getLatenessFrequencies(connStr,"SOTON","WATRLMN",2)
+totalFrequency = 0
+sum = 0.0
+for l in allLate:
+    totalFrequency += int((l.split(",")[1]))
+for l in allLate:
+    sum += float(int(l.split(",")[0]) * int(l.split(",")[1]) / totalFrequency)
+print(sum)
