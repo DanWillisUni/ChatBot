4faa00d0ba5f9668d6fe66288e781929e6eeacc3
DanWillisUni <dan.willis.uni@gmail.com>
2022-01-27 20:31:13 +0000

    re formatted all the camelcases

--- src/PartTwo/Helpers/SPHelper.py
+++ src/PartTwo/Helpers/SPHelper.py
@@ -1,88 +1,91 @@
-import PartTwo.Helpers.DB as DB
+import PartTwo.Helpers.DB as db
 
-def getAllDatatOnStation(connStr,station):
+
+def get_all_data_on_station(conn_str, station):
     """
     Runs the GetAllInfoOnStationTimes stored procedure with the parameters
 
-    :param connStr:
+    :param conn_str:
     Connection string to the Server/Database
     :param station:
     Station name to get all the data on
     :return:
     The results for the SP
     """
-    query = "EXEC GetAllInfoOnStationTimes @station = '" + station + "'"  #sets the query string
-    result = DB.runQuery(connStr, query)  #execute the stored procedure
+    query = "EXEC GetAllInfoOnStationTimes @station = '" + station + "'"  # sets the query string
+    result = db.run_query(conn_str, query)  # execute the stored procedure
     return result
 
-def getLatenessFromStations(connStr,fromStation,toStation,rangeStart,rangeEnd):
+def get_lateness_from_stations(conn_str, from_station, to_station, range_start, range_end):
     """
     Runs the GetLatenessFromStations stored procedure with the parameters
 
-    :param connStr:
+    :param conn_str:
     Connection string of the server/database
-    :param fromStation:
+    :param from_station:
     The station A
-    :param toStation:
+    :param to_station:
     The station B
-    :param rangeStart:
+    :param range_start:
     The range of late by start
-    :param rangeEnd:
+    :param range_end:
     The range of late by end
     :return:
     String array of all the rows from the query
     """
-    query = "EXEC GetLatenessFromStations @FromStation = '" + fromStation +"',@ToStation = '"+toStation+"',@LateByRangeStart = "+str(rangeStart)+",@LateByRangeEnd = " + str(rangeEnd)  # generate query
-    result = DB.runQuery(connStr, query)  #run the query
+    query = "EXEC GetLatenessFromStations @FromStation = '" + from_station + "',@ToStation = '" + to_station + "',@LateByRangeStart = " + str(range_start) + ",@LateByRangeEnd = " + str(range_end)  # generate query
+    result = db.run_query(conn_str, query)  #run the query
     return result
 
-def getLatenessOfBoth(connStr, fromStation, toStation):
+
+def get_lateness_of_both(conn_str, from_station, to_station):
     """
     Executes the GetLatenessOfBoth stored procedure
 
-    :param connStr:
+    :param conn_str:
     The connection string to the server/database
-    :param fromStation:
+    :param from_station:
     The Station A name
-    :param toStation:
+    :param to_station:
     The station B name
     :return:
     String array of the result rows
     """
-    query = "EXEC GetLatenessOfBoth @FROM = '" + fromStation + "',@TO = '" + toStation + "'"  # generate the query string
-    result = DB.runQuery(connStr, query)  #run the query on the database
+    query = "EXEC GetLatenessOfBoth @FROM = '" + from_station + "',@TO = '" + to_station + "'"  # generate the query string
+    result = db.run_query(conn_str, query)  # run the query on the database
     return result
 
-def compareStations(connStr, A, B):
+
+def compare_stations(conn_str, a, b):
     """
     Executes the CompareStations stored procedure with parameters
 
-    :param connStr:
+    :param conn_str:
     Connection string to database/Server
-    :param A:
+    :param a:
     Station Name A
-    :param B:
+    :param b:
     Station Name B
 
     :return:
     String array of the rows of results
     """
-    query = "EXEC CompareStations @A = '" + A + "',@B = '" + B + "'"  # generate the query
-    result = DB.runQuery(connStr, query)  # run the query
+    query = "EXEC CompareStations @A = '" + a + "',@B = '" + b + "'"  # generate the query
+    result = db.run_query(conn_str, query)  # run the query
     return result[0][:-2]
 
-def getLatenessFromRID(connStr,RID):
+def get_lateness_from_rid(conn_str, rid):
     """
     Execute the GetLatenessFromRID stored procedure
 
-    :param connStr:
+    :param conn_str:
     Connection string to the database or server
-    :param RID:
+    :param rid:
     Route ID to search
 
     :return:
     String array of the result rows
     """
-    query = "EXEC GetLatenessFromRID @rid = '" + RID + "'"  # get the query string
-    result = DB.runQuery(connStr, query)  # run the query
+    query = "EXEC GetLatenessFromRID @rid = '" + rid + "'"  # get the query string
+    result = db.run_query(conn_str, query)  # run the query
     return result
--- src/PartTwo/Tests/PartTwoUnitTests.py
+++ src/PartTwo/Tests/PartTwoUnitTests.py
@@ -1,20 +1,55 @@
 import unittest
 import PartTwo.Bayes as b
+import PartTwo.KNearestNeighbours as knn
+import PartTwo.NeuralNetwork as nn
+import PartTwo.Helpers.ProbabilityHelper as ph
+import PartTwo.Helpers.Fitness as f
+
 
 class MyTestCase(unittest.TestCase):
-    def test_Bayes_calculation(self):
+
+    # test fitness
+    def test_get_all_stations(self):
+        self.assertEqual(True, True)
+
+    def test_sim_result(self):
+        self.assertEqual(True, True)
+
+    def test_get_rid_data(self):
+        self.assertEqual(True, True)
+
+    # test probability helper
+    def test_probability_from_frequency(self):
+        test_cases = [
+            [["0,10", "1,10", "2,5", "3,5", "4,5", "5,5"], 2, 0.5]
+        ]
+        for i in test_cases:
+            result = ph.probability_from_frequency(i[0], i[1])
+            self.assertEqual(i[2], result)
+
+    def test_get_q(self):
+        self.assertEqual(True, True)
+
+    def test_get_outliers_min(self):
+        self.assertEqual(True, True)
+
+    # test predicitons
+    def test_bayes_calculation(self):
         #http://psych.fullerton.edu/mbirnbaum/bayes/bayescalc.html
-        toTest = [[0.5,0.4,0.1,0.8],
+        test_cases = [[0.5,0.4,0.1,0.8],
             [0.1,0.2,0.1,0.18181818181818182],
             [0.9,0.2,0.8,0.6923076923076924],
             [0.4,0.7,0.3,0.6086956521739131],
             [0.1818181,0.6923076,0.6086956,0.20175428971863218]]
-        for i in toTest:
-            result = b.bayesTheorem(i[0], i[1], i[2])
-            self.assertEqual(i[3],result)
-    def test_ProbabilityHelper(self):
-        self.assertEqual(True,True)
+        for i in test_cases:
+            result = b.bayes_theorem(i[0], i[1], i[2])
+            self.assertEqual(i[3], result)
 
+    def test_knn(self):
+        self.assertEqual(True, True)
+
+    def test_nn(self):
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
