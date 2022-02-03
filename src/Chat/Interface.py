1044de58fd82a4deaa8f7da957f6021d8a024933
Brandon Gous <fierchecer@gmail.com>
2022-02-03 17:32:48 +0000

    Implement pyqt ui

--- src/Chat/Interface.py
+++ src/Chat/Interface.py
@@ -1,11 +1,34 @@
+from PyQt5.QtCore import QThread, QObject, pyqtSlot, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QLineEdit, QGridLayout
 import datetime
+
+from src.Engine import KEngine
+
+from pymitter import EventEmitter
+
+eventEmitter = EventEmitter()
+
+class EngineWorker(QObject):
+
+    def __init__(self, ui):
+        super(EngineWorker, self).__init__()
+
+        self.engine = KEngine(ui)
+        self.start.connect(self.run)
+
+    start = pyqtSignal(str)
+
+    @pyqtSlot()
+    def run(self):
+        self.engine.reset()
+        self.engine.run()
 
 
 class ChatBotWindow(QWidget):
 
     def __init__(self):
         super(ChatBotWindow, self).__init__()
+
         self.text_area = QTextEdit()
         self.chat_line = QLineEdit()
         self.send_button = QPushButton("Send")
@@ -15,17 +38,32 @@ class ChatBotWindow(QWidget):
         self.setFixedWidth(400)
         self.setLayout(Layout(self.text_area, self.chat_line, self.send_button))
 
-        self.send_button.clicked.connect(lambda: self.process_send("You"))
-        self.chat_line.returnPressed.connect(self.send_button.click)
-
         self.show()
 
-    def process_send(self, sender):
+        # https://stackoverflow.com/questions/20324804/how-to-use-qthread-correctly-in-pyqt-with-movetothread
+        self.engine_thread = QThread()
+        self.engine_thread.start()
+
+        self.engine_worker = EngineWorker(self)
+
+        self.send_button.clicked.connect(lambda: self.engine_worker.engine.set_input(self.process_response("You")))
+        self.chat_line.returnPressed.connect(self.send_button.click)
+
+        self.engine_worker.moveToThread(self.engine_thread)
+
+        self.engine_worker.start.emit("hello")
+
+    def send_message(self, sender, message):
         now = datetime.datetime.now()
         self.text_area.append(sender + ': (' + str(now.hour) + ':' + str(now.minute) + ')')
-        self.text_area.append('    ' + self.chat_line.text())
+        self.text_area.append('    ' + message)
         self.text_area.append('')
+
+    def process_response(self, sender):
+        response = self.chat_line.text()
+        self.send_message(sender, response)
         self.chat_line.clear()
+        return response
 
 
 class Layout(QGridLayout):
